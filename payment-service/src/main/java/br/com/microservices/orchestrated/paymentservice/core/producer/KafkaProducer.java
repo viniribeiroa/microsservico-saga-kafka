c7cf26bba9bacd3e0749140a3bad32a0975b9152
Vinicius Ribeiro <viniribeiroa@gmail.com>
2026-03-13 21:40:21 -0300

    KAFKA Producer

---
+++ payment-service/src/main/java/br/com/microservices/orchestrated/paymentservice/core/producer/KafkaProducer.java
@@ -0,0 +1,29 @@
+package br.com.microservices.orchestrated.paymentservice.core.producer;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Component;
+
+/**
+ * Criado por VINICIUS em 13/03/2026
+ * base_files
+ */
+@Slf4j
+@RequiredArgsConstructor
+@Component
+public class KafkaProducer {
+    private final KafkaTemplate<String, String> kafkaTemplate;
+    @Value("${spring.kafka.topic.orchestrator}")
+    private String orchestratorTopic;
+
+    public void sendEvent(String payload){
+        try{
+            log.info("Sending event to topic {} with data {}", orchestratorTopic, payload);
+            kafkaTemplate.send(orchestratorTopic, payload);
+        }catch (Exception e){
+            log.error("Error try to send data to topic {} with data {}", orchestratorTopic, payload, e);
+        }
+    }
+}
