c55e22034b701b0eb70ed05634d4ac5600a4c416
Vinicius Ribeiro <viniribeiroa@gmail.com>
2026-03-24 23:09:29 -0300

    Inventory v2

---
+++ inventory-service/src/main/java/br/com/microservices/orchestrated/inventoryservice/core/service/InventoryService.java
@@ -0,0 +1,122 @@
+package br.com.microservices.orchestrated.inventoryservice.core.service;
+
+import br.com.microservices.orchestrated.inventoryservice.config.exception.ValidationException;
+import br.com.microservices.orchestrated.inventoryservice.core.dto.Event;
+import br.com.microservices.orchestrated.inventoryservice.core.dto.History;
+import br.com.microservices.orchestrated.inventoryservice.core.dto.Order;
+import br.com.microservices.orchestrated.inventoryservice.core.dto.OrderProducts;
+import br.com.microservices.orchestrated.inventoryservice.core.model.Inventory;
+import br.com.microservices.orchestrated.inventoryservice.core.model.OrderInventory;
+import br.com.microservices.orchestrated.inventoryservice.core.producer.KafkaProducer;
+import br.com.microservices.orchestrated.inventoryservice.core.repository.InventoryRepository;
+import br.com.microservices.orchestrated.inventoryservice.core.repository.OrderInventoryRepository;
+import br.com.microservices.orchestrated.inventoryservice.core.utils.JsonUtil;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+import static br.com.microservices.orchestrated.inventoryservice.core.enums.ESagaStatus.SUCCESS;
+
+/**
+ * Criado por VINICIUS em 24/03/2026
+ * base_files
+ */
+@Slf4j
+@Service
+@AllArgsConstructor
+public class InventoryService {
+
+    private static final String CURRENT_SOURCE = "INVENTORY_SERVICE";
+
+    private final JsonUtil jsonUtil;
+    private final KafkaProducer producer;
+    private final InventoryRepository inventoryRepository;
+    private final OrderInventoryRepository orderInventoryRepository;
+
+    public void updateInventory(Event event){
+
+        try {
+            checkCurrentValidation(event);
+            createOrderInvetory(event);
+            updateInventory(event.getPayload());
+            handleSuccess(event);
+        } catch (Exception e) {
+            log.error("Error trying to update inventory", e);
+
+        }
+        producer.sendEvent(jsonUtil.toJson(event));
+    }
+
+    private void checkCurrentValidation(Event event){
+        if (orderInventoryRepository.existsByOrderIdAndTransactionId(event.getOrderId(), event.getTransactionId())){
+            throw new ValidationException("there´s another transaction for this validation");
+        }
+    }
+
+    private void createOrderInvetory(Event event){
+        event
+                .getPayload()
+                .getProducts()
+                .forEach(products ->{
+                    var inventory = findInventoryByProductCode(products.getProduct().getCode());
+                    var orderInventory = createOrderInventory(event,products,inventory);
+                    orderInventoryRepository.save(orderInventory);
+                });
+    }
+
+    private OrderInventory createOrderInventory(Event event, OrderProducts products, Inventory inventory){
+        return OrderInventory
+                .builder()
+                .inventory(inventory)
+                .oldQuantity(inventory.getAvailable())
+                .orderQuantity(products.getQuantity())
+                .newQuantity(inventory.getAvailable() - products.getQuantity())
+                .orderId(event.getPayload().getId())
+                .transactionId(event.getTransactionId())
+                .build();
+
+    }
+
+    private void updateInventory(Order order){
+        order
+                .getProducts()
+                .forEach(products ->{
+                    var inventory = findInventoryByProductCode(products.getProduct().getCode());
+                    checkInventory(inventory.getAvailable(), products.getQuantity());
+                    inventory.setAvailable(inventory.getAvailable() - products.getQuantity());
+                    inventoryRepository.save(inventory);
+                });
+    }
+    private void checkInventory(int available, int orderQuantity){
+        if (orderQuantity > available){
+            throw new ValidationException("Product is out of stock");
+        }
+    }
+
+    private void handleSuccess(Event event) {
+        event.setStatus(SUCCESS);
+        event.setSource(CURRENT_SOURCE);
+        addHistory(event, "Inventory updated successfully!");
+    }
+
+    private void addHistory(Event event, String message) {
+        var history = History
+                .builder()
+                .source(event.getSource())
+                .status(event.getStatus())
+                .message(message)
+                .createdAt(LocalDateTime.now())
+                .build();
+        event.addToHistory(history);
+    }
+
+    private Inventory findInventoryByProductCode(String productCode){
+        return inventoryRepository
+                .findByProductCode(productCode)
+                .orElseThrow(() -> new ValidationException("Inventory not found by informed product"));
+    }
+
+
+}
