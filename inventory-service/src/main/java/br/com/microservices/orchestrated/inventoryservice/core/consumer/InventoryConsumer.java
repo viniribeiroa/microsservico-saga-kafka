64ef20cceba51c80328bb563a715645cc17b9c79
Vinicius Ribeiro <viniribeiroa@gmail.com>
2026-03-25 20:55:01 -0300

    Inventory v3

--- inventory-service/src/main/java/br/com/microservices/orchestrated/inventoryservice/core/consumer/InventoryConsumer.java
+++ inventory-service/src/main/java/br/com/microservices/orchestrated/inventoryservice/core/consumer/InventoryConsumer.java
@@ -3,6 +3,7 @@ package br.com.microservices.orchestrated.inventoryservice.core.consumer;
 
 
 
+import br.com.microservices.orchestrated.inventoryservice.core.service.InventoryService;
 import br.com.microservices.orchestrated.inventoryservice.core.utils.JsonUtil;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -18,6 +19,7 @@ import org.springframework.stereotype.Component;
 @AllArgsConstructor
 public class InventoryConsumer {
 
+    private final InventoryService inventoryService;
     private final JsonUtil jsonUtil;
 
     @KafkaListener(
@@ -27,7 +29,8 @@ public class InventoryConsumer {
     public void consumeSuccessEvent(String payload){
         log.info("Receiving success event {} from inventory-success topic", payload);
         var event = jsonUtil.toEvent(payload);
-        log.info(event.toString());
+        inventoryService.updateInventory(event);
+
     }
 
     @KafkaListener(
@@ -37,7 +40,7 @@ public class InventoryConsumer {
     public void consumeFailEvent(String payload){
         log.info("Receiving rollback event {} from inventory-fail topic", payload);
         var event = jsonUtil.toEvent(payload);
-        log.info(event.toString());
+        inventoryService.rollbackInventory(event);
     }
 
 }
--- inventory-service/src/main/java/br/com/microservices/orchestrated/inventoryservice/core/service/InventoryService.java
+++ inventory-service/src/main/java/br/com/microservices/orchestrated/inventoryservice/core/service/InventoryService.java
@@ -17,7 +17,7 @@ import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
 
-import static br.com.microservices.orchestrated.inventoryservice.core.enums.ESagaStatus.SUCCESS;
+import static br.com.microservices.orchestrated.inventoryservice.core.enums.ESagaStatus.*;
 
 /**
  * Criado por VINICIUS em 24/03/2026
@@ -44,7 +44,7 @@ public class InventoryService {
             handleSuccess(event);
         } catch (Exception e) {
             log.error("Error trying to update inventory", e);
-
+            handleFailCurrentNotExecuted(event, e.getMessage());
         }
         producer.sendEvent(jsonUtil.toJson(event));
     }
@@ -110,6 +110,38 @@ public class InventoryService {
                 .createdAt(LocalDateTime.now())
                 .build();
         event.addToHistory(history);
+    }
+
+    private void handleFailCurrentNotExecuted(Event event, String message) {
+        event.setStatus(ROLLBACK_PENDING);
+        event.setSource(CURRENT_SOURCE);
+        addHistory(event, "Fail to update inventory: ".concat(message));
+    }
+
+    public void rollbackInventory(Event event){
+        event.setStatus(FAIL);
+        event.setSource(CURRENT_SOURCE);
+        try{
+            returnInventoryToPreviousValues(event);
+            addHistory(event, "Rollback executed for Inventory!");
+        } catch (Exception e) {
+            addHistory(event, "Rollback not executed for Inventory: ".concat(e.getMessage()));
+        }
+        producer.sendEvent(jsonUtil.toJson(event));
+    }
+
+    private void returnInventoryToPreviousValues(Event event){
+        orderInventoryRepository
+                .findByOrderIdAndTransactionId(event.getPayload().getId(), event.getTransactionId())
+                .forEach(orderInventory -> {
+                    var inventory = orderInventory.getInventory();
+                    inventory.setAvailable(orderInventory.getOldQuantity());
+                    inventoryRepository.save(inventory);
+                    log.info("Restored inventory for order {} from {} to {}",
+                            event.getPayload().getId(),
+                            orderInventory.getNewQuantity(),
+                            inventory.getAvailable());
+                });
     }
 
     private Inventory findInventoryByProductCode(String productCode){
